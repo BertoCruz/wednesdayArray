48efc881f49e303a6819b6e09f3e6ba6128bd444
Dani Lopez-Cruz <roberto.lc2493@gmail.com>
2022-06-08 22:13:19 -0600

    Add method calls for the array objects

--- arrays.js
+++ arrays.js
@@ -1,3 +1,19 @@
 var friends = ["Mansour", "Nico", "Adolfo", "Rachel"];
 var friendYears = [10, 5, 5, 6];
 var isDj = [true, true, false, false];
+
+
+//below will remove/return the first element of the friends array
+//return: Mansour
+friends.shift();
+
+
+//below will remove/return the last element of the friendYears array.
+//return: 6
+friendYears.pop();
+
+
+//below will return the "start", index [0], all the way up to
+// (but excluding anything after) the "end", index [2] of the isDj array.
+//return: [true,true]
+isDJ.slice(0,2);
